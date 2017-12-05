d0812c8e6ae5461d51a79f0f705093d16c2d4da8
Iurii Pavliuk <iurii@playsnak.com>
2017-12-05 16:12:34 +0100

    - editor script for RealmsYouTubePlayback script

---
+++ Assets/Realms/Editor/RealmsYouTubePlaybackEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+// the pupropse of this script is to hide public fields from HighQualityPlayback class
+[CustomEditor(typeof(PlaysnakRealms.RealmYouTubePlayback))]
+
+public class RealmsYouTubePlaybackEditor : Editor {
+
+	void OnEnable () {
+	}
+
+	public override void OnInspectorGUI() {
+		EditorGUILayout.HelpBox ("All the public fields of HighQualityPlayback are hidden and will be set automatically", MessageType.Info);
+	}
+}
